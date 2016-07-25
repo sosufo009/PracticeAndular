46e19a2cea194248083636360be77ca4f20c57e3
RyanLion <ryanlion@Ryan-Lion-Mac.fritz.box>
2016-07-25 18:27:36 +0200

    fdfsd

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ app.post('/api/nodes/:name', function (request, response)
 {
     pg.connect(process.env.DATABASE_URL, function(err, client, done)
     {
-        console.log('insert into nodes(label,del) values('+request.name+',ture);');
+        console.log('insert into nodes(label,del) values('+request.params.name+',ture);');
 
-        client.query('insert into nodes(label,del) values('+request.name+',ture);', function(err, result)
+        client.query('insert into nodes(label,del) values('+request.params.name+',ture);', function(err, result)
         {
             done();
             response.send(result);
