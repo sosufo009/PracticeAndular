99a593de0d21375f4fbd69b3e3eda6305b2f1dde
RyanLion <ryanlion@Ryan-Lion-Mac.fritz.box>
2016-07-25 16:32:11 +0200

    fsdfsd

--- index.js
+++ index.js
@@ -18,13 +18,20 @@ app.get('/', function(request, response) {
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM nodes', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
+        done();
+        console.log(result);
+        response.send(result);
+        // if (err)
+        // {
+        //     console.error(err);
+        //     response.send("Error " + err);
+        // }
+        // else
+        // {
+        //     response.render('pages/db', {results: result.rows} );
+        // }
     });
-  });
+});
 })
 
 
